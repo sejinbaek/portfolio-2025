6b29065161450dff28828d5aea364bf088f35047
sejinbaek <sejin9767@gmail.com>
2025-10-17 10:38:13 +0900

    feat: loading-spinner 적용

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,11 @@
+import { HashLoader } from "react-spinners";
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div className="scale-90 min-[400px]:scale-100 md:scale-110 lg:scale-120">
+        <HashLoader color="#262626" loading size={36} />
+      </div>
+    </div>
+  );
 }
