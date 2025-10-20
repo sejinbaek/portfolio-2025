c97093372a5bf3252193819fd7b64353a3efd6ec
sejinbaek <sejin9767@gmail.com>
2025-10-20 16:46:12 +0900

    style: project 커버 이미지 호버 애니메이션 적용

--- src/app/projects/components/ProjectCard.tsx
+++ src/app/projects/components/ProjectCard.tsx
@@ -11,36 +11,35 @@ export default function ProjectCard({
 }) {
   const { slug, cover, title, subtitle, description } = project;
   return (
-    <article className="">
+    <article>
       <Link
         href={`/projects/${slug}`}
         aria-label={`${title} 상세 보기`}
         className="block"
       >
-        <div className="relative aspect-3/2 overflow-hidden rounded-xl">
+        <div className="relative aspect-3/2 overflow-hidden rounded-2xl group shadow-sm transition-shadow duration-500 ease-[cubic-bezier(.5,.8,.2,1)] hover:shadow-xl">
           <Image
             src={cover.src}
             alt={cover.alt || title}
             fill
-            className="object-cover transition-transform duration-300 hover:scale-110
-          will-change-transform"
+            className="object-cover transform-gpu will-change-transform transition-transform duration-500 ease-[cubic-bezier(.5,.8,.2,1)] hover:scale-105"
             sizes="(min-width:1024px) 33vw, (min-width:768px) 50vw, 100vw"
             priority={priority} // 첫 카드만 true
           />
         </div>
       </Link>
 
-      <div className="pt-4 xl:pt-10 text-neutral-800">
+      <div className="pt-4 xl:pt-10 text-neutral-800 dark:text-white">
         <h3 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold">
           {title}
         </h3>
         {subtitle && (
-          <p className="py-2 lg:pt-4 font-medium text-sm min-[360px]:text-base md:text-lg lg:text-xl">
+          <p className="py-2 lg:pt-4 font-semibold text-sm min-[360px]:text-base md:text-lg lg:text-xl text-neutral-600">
             {subtitle}
           </p>
         )}
         {description && (
-          <p className="text-sm min-[360px]:text-base md:text-lg xl:text-xl xl:leading-8">
+          <p className="text-sm min-[360px]:text-base md:text-lg xl:text-xl xl:leading-8 text-neutral-500">
             {description}
           </p>
         )}
