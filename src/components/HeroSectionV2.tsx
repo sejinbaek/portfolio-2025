045c2b476fb068dc8d9eebd9cda9036009372fca
sejinbaek <sejin9767@gmail.com>
2025-12-25 22:02:13 +0900

    refactor: herosection span -> p 태그로 변경

--- src/components/HeroSectionV2.tsx
+++ src/components/HeroSectionV2.tsx
@@ -24,16 +24,14 @@ export default function HeroSectionV2() {
 
       {/* ✅ 콘텐츠는 위로 */}
       <div className="relative z-10 h-full flex flex-col justify-center items-center space-y-12 md:space-y-24">
-        <span id="hero-title" className={clsx("font-paperlogy", subTextStyle)}>
+        <p id="hero-title" className={clsx("font-paperlogy", subTextStyle)}>
           [ BAEK SEJIN - 2025 ]
-        </span>
+        </p>
 
-        <h1 className={clsx(shrikhand.className, mainTextStyle)}>portfolio</h1>
-        <span
-          className={clsx("font-paperlogy", subTextStyle, "leading-relaxed")}
-        >
+        <p className={clsx("font-shrikhand", mainTextStyle)}>portfolio</p>
+        <p className={clsx("font-paperlogy", subTextStyle, "leading-relaxed")}>
           Web Publisher, <br className="lg:hidden" /> Frontend Developer
-        </span>
+        </p>
       </div>
     </section>
   );
